48abd52a7c0c4404f555db63589597aea8f5c262
devsazzad <suzon956@gmail.com>
2022-03-24 08:55:18 -0600

    practice 2

---
+++ practice.php
@@ -0,0 +1,15 @@
+<?php
+hello word
+
+/*case study:
+    let's introduce myself
+    I am sazzadul islam sujon. I am 20 years old. I am from Sylhet, Moulvibazar.
+*/
+$name = "sazzadul islam sujon"; // datatype is strang
+$id = 80; // data type is integer
+$age = 21.2; // data type is double / float
+$web_designer = false; // data type is bolean
+$php_laravel_developer = true; // datatype is bolean
+$from = "sylhet"// datatype is strang
+
+?>
